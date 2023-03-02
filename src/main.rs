19661e6cc1c9cee9b4ab05a6f13aa77dc2ff4324
WinstonMuijs <winstonhmuijs@gmail.com>
2023-03-02 11:28:54 +0100

    Guessing Game of the Rust handbook. Tweaked by me, a little different than the code used in the handbook.

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::{error::Error, io::stdin};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Welkom bij: Raad Het Getal Spel!!");
+
+    loop {
+        println!("Wat is het geheime getal? Voor het hier in...");
+        let mut gok = String::new();
+        stdin().read_line(&mut gok)?;
+
+        let gok: u32 = match gok.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Verkeerd getal!");
+                continue;
+            }
+        };
+
+        match gok.cmp(&secret_number) {
+            Ordering::Less => println!("Te Laag!"),
+            Ordering::Greater => println!("Te Hoog!"),
+            Ordering::Equal => {
+                println!("Je hebt het getal geraden! Je wint het spel...");
+                break;
+            }
+        }
+    }
+    Ok(())
+}
